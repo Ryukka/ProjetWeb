c54cb9a2944690d59f2bb8250d68fb5b0a89be7c
Ryukka <yven.shue@edu.ece.fr>
2019-10-07 15:51:48 +0200

    modif Connections.js

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Connection} from './src/datas/Connections.js';
+import Connection from './datas/Connections.js';
 
 function App() {
     return (
       <div>
       <Connection/>
       </div>
-
     );
 }
 
